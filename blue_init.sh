157b31f439e8528f756be3fe496c6602ede10ae9
Dan Isla <dan.isla@gmail.com>
2017-01-21 11:48:31 -0800

    added blue_init script

---
+++ blue_init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Attaching hci0..."
+if ! /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -; then
+    echo "First try failed. Let's try another time."
+    /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -
+fi
+
+echo "Bring hci0 up..."
+hciconfig hci0 up
